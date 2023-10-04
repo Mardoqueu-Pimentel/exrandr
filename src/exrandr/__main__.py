8304e2a81e16b67ff74b5f88476dc896de2995dd
Mardoqueu Pimentel <mardoqueu.pimentel@gmail.com>
2023-10-04 06:48:15 -0400

    wip

--- src/exrandr/__main__.py
+++ src/exrandr/__main__.py
@@ -1,107 +1,108 @@
 #!/usr/bin/env python3
 from __future__ import annotations
 
-import itertools
-import math
 import os
-import re
+from collections import Counter
 
-import attr
+import attrs
 import click
-from attrs import define
+from prettyprinter import cpprint
 
 
-@define
+@attrs.define
+class Resolution:
+    width: float
+    height: float
+
+    class ParamType(click.ParamType):
+        name = "Resolution"
+
+        def convert(
+            self, value: Resolution | str, param: click.Parameter | None, ctx: click.Context | None
+        ) -> Resolution:
+            if isinstance(value, Resolution):
+                return value
+
+            try:
+                width, sep, height = value.partition("x")
+                if sep != "x":
+                    raise ValueError("'x' separator not found")
+                return Resolution(width=float(width), height=float(height))
+            except ValueError as exc:
+                self.fail(f"{value!r} is not a valid {self.name} -> {exc!r}", param, ctx)
+                raise
+
+    aspect_ratio: float = attrs.field()
+
+    @aspect_ratio.default
+    def _(self):
+        return self.width / self.height
+
+
+@attrs.define
 class Display:
     name: str
-    physical_diagonal: float
-
-    height: float
-    aspect_ratio: float
-    viewport_height: float
-    viewport_aspect_ratio: float
-
-    zoom: float
-
+    inches: float
+    viewport: Resolution
     gamma: float
+    zoom: float
     rotation: str
     primary: bool
+    default_ppi: bool
 
-    def __attrs_post_init__(self):
-        if self.rotation in ("left", "right"):
-            self.viewport_height = self.viewport_width
-            self.viewport_aspect_ratio = 1 / self.viewport_aspect_ratio
+    ppi: float = attrs.field()
 
-    @property
-    def width(self):
-        return self.height * self.aspect_ratio
+    @ppi.default
+    def _(self):
+        n = self.viewport.height * (1 + self.viewport.aspect_ratio ** 2) ** (1 / 2)
+        return n / self.inches
 
-    @property
-    def viewport_width(self):
-        return self.viewport_height * self.viewport_aspect_ratio
+    vr: Resolution = attrs.field()
 
-    @property
-    def ppi(self):
-        n = self.height * (1 + self.aspect_ratio ** 2) ** (1 / 2)
-        return n / self.physical_diagonal
-
-    @property
-    def viewport_ratio(self):
-        return self.zoomed_viewport_height / (self.width if self.rotation else self.height)
-
-    @property
-    def zoomed_viewport_height(self):
-        return self.viewport_height / self.zoom
-
-    @property
-    def zoomed_viewport_width(self):
-        return self.viewport_width / self.zoom
-
-    @classmethod
-    def from_str(cls, s: str):
-        def unpack_and_make(
-            name: str, inches: str, res: str, zoom: str = "1.0",
-            gamma: str = "1.0",
-            rotation: str | None = None, primary: str | None = None
-        ):
-            width, height = map(float, res.split("x"))
-            return Display(
-                name=name,
-                physical_diagonal=float(inches),
-                height=height,
-                aspect_ratio=width / height,
-                viewport_height=height,
-                viewport_aspect_ratio=width / height,
-                zoom=float(zoom),
-                gamma=float(gamma),
-                rotation=rotation,
-                primary=eval(primary) if primary else False,
-            )
-
-        items = [arg.split("=") for arg in re.split(r", +| +", s)]
-        kwargs = {k: v for k, v in items}
-        return unpack_and_make(**kwargs)
-
-    def ppi_scale(self, ppi: float):
-        height = ppi * self.physical_diagonal / (1 + self.aspect_ratio ** 2) ** (1 / 2)
-        return attr.evolve(self, viewport_height=self.viewport_height * (height / self.height))
-
-    def ui_scale(self, scale: float):
-        return attr.evolve(self, viewport_height=self.viewport_height * scale)
+    @vr.default
+    def _(self):
+        return self.viewport
 
     def as_dict(self):
-        return attr.asdict(self)
+        return attrs.asdict(self)
+
+    vppi: float = attrs.field()
+
+    @vppi.default
+    def _(self):
+        n = self.vr.height * (1 + self.vr.aspect_ratio ** 2) ** (1 / 2)
+        return n / self.inches
+
+    zoomed_vr_viewport_ratio: float = attrs.field()
+
+    @zoomed_vr_viewport_ratio.default
+    def _(self):
+        k = self.vr.height / self.viewport.height
+        return k / self.zoom
+
+    def scale(self, f: float):
+        return Display(
+            name=self.name,
+            inches=self.inches,
+            viewport=self.viewport,
+            gamma=self.gamma,
+            zoom=self.zoom,
+            rotation=self.rotation,
+            primary=self.primary,
+            default_ppi=self.default_ppi,
+            vr=Resolution(width=self.vr.width * f, height=self.vr.height * f),
+        )
 
     def as_xrandr_args(self, position: int):
         yield "--output"
         yield self.name
         yield "--mode"
-        yield f"{self.width:.0f}x{self.height:.0f}"
+        yield f"{self.viewport.width:.0f}x{self.viewport.height:.0f}"
         if self.rotation:
             yield "--rotate"
             yield self.rotation
         yield "--scale"
-        yield f"{self.viewport_ratio}"
+        yield f"{self.zoomed_vr_viewport_ratio}"
         yield "--pos"
         yield f"{position}x0"
         yield "--gamma"
@@ -109,69 +110,75 @@ class Display:
         if self.primary:
             yield "--primary"
 
-    def __repr__(self, properties=()):
-        properties = properties or [
-            k for
-            k, v in self.__class__.__dict__.items()
-            if isinstance(v, property)
-        ]
 
-        d = self.as_dict()
-        keys = itertools.chain(d.items(), [(k, getattr(self, k)) for k in properties])
-
-        if len(attributes := ", ".join(f"{k}={v}" for k, v in keys)) >= 80:
-            attributes = attributes.replace(", ", "\n\t")
-
-        if len(result := f"{self.__class__.__name__}({attributes})") >= 80:
-            result = f"{self.__class__.__name__}(\n\t{attributes}\n)\n"
-
-        return result
+@click.group(chain=True)
+@click.option("--scale", "-s", type=float, default=1.0, show_default=True)
+@click.option("--ppi", "-p", type=float, default=None, show_default=True)
+@click.option("--apply", "-a", is_flag=True, default=False, show_default=True)
+@click.option("--verbose", "-v", is_flag=True, default=False, show_default=True)
+def main(scale: float, ppi: float | None, apply: bool, verbose: bool):
+    pass
 
 
-def int_or_float(x: str):
-    try:
-        return int(x)
-    except Exception:
-        return float(x)
+@main.command("display")
+@click.option("--name", "-n", type=str, required=True)
+@click.option("--inches", "-i", type=float, required=True)
+@click.option("--resolution", "-r", type=Resolution.ParamType())
+@click.option("--gamma", "-g", type=float, default=1.0, show_default=True)
+@click.option("--zoom", "-z", type=float, default=1.0, show_default=True)
+@click.option("--rotation", "-o", type=str, default="", show_default=True)
+@click.option("--primary", "-p", is_flag=True, default=False, show_default=True)
+@click.option("--default-ppi", is_flag=True, default=False, show_default=True)
+def process_display(
+    name: str,
+    inches: float,
+    resolution: Resolution,
+    gamma: float,
+    zoom: float,
+    rotation: str | None,
+    primary: bool,
+    default_ppi: bool,
+):
+    return Display(
+        name=name,
+        inches=inches,
+        viewport=resolution,
+        gamma=gamma,
+        zoom=zoom,
+        rotation=rotation,
+        primary=primary,
+        default_ppi=default_ppi,
+    )
 
 
-@click.command()
-@click.option("--scale", default=1, type=int)
-@click.option(
-    "--display",
-    "displays",
-    required=True,
-    multiple=True,
-    help="""
-    Format: 
-        S -> "name=<str>" "inches=<int>" "res=<3840>x<2160>" [OPT...]
-        OPT -> ZOOM | GAMMA | ROTATION | PRIMARY
-        ZOOM -> "zoom=<float>"
-        GAMMA -> "gamma=<float>"
-        ROTATION -> "rotation=<float>"
-        PRIMARY -> "primary=True"
-    """,
-    callback=lambda _, __, xs: [Display.from_str(x) for x in xs],
-)
-@click.option("--ppi", type=int_or_float, default="1")
-@click.option("--apply", is_flag=True, default=False)
-@click.option("-v", "--verbose", is_flag=True, default=False)
-def main(scale: int, displays: list[Display], ppi: float | int, apply: bool, verbose: bool):
-    assert ppi is None or ppi > 0
+@main.result_callback()
+def process_displays(
+    displays: list[Display], scale: float, ppi: float | None, apply: float, verbose: bool
+):
+    displays_aspect_ratios = {d.viewport.aspect_ratio for d in displays}
+    primary_displays = [d for d in displays if d.primary]
+    default_ppi_displays = [d for d in displays if d.default_ppi]
+
+    assert len(displays_aspect_ratios) == 1, "All displays must have the same aspect ratio"
+    assert len(primary_displays) == 1, "One display must be primary"
+    assert len(default_ppi_displays) <= 1, "At most one display must be default_ppi"
+
+    displays = [d.scale(scale) for d in displays]
+
+    if ppi is None:
+        ppi = default_ppi_displays and default_ppi_displays[0].ppi or min(d.ppi for d in displays)
+    displays = [d.scale(ppi / d.ppi) for d in displays]
 
     cmd = ["xrandr"]
     position = 0
-
-    if isinstance(ppi, int):
-        ppi = displays[ppi - 1].ppi
-
-    displays = [display.ui_scale(scale).ppi_scale(ppi) for display in displays]
-
     for display in displays:
         if verbose:
-            print(display)
+            cpprint(display)
         cmd.append(" ".join(display.as_xrandr_args(position)))
-        position += round(display.zoomed_viewport_width)
+        position += round(
+            (display.viewport.height if display.rotation else display.viewport.width)
+            * display.zoomed_vr_viewport_ratio
+        )
 
     result = " \\\n\t".join(cmd) + ("" if apply else " | :")
     click.echo(result)
